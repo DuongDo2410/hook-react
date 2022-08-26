3a6c55f0ff6c90736b215bc444183a5337fd09b2
duongdo2410 <duongdo2k1@gmail.com>
2022-08-26 09:25:41 +0000

    done action up and down button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import React from "react";
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: 0
+    };
+    this.incremnet = this.incremnet.bind(this);
+    this.decremnet = this.decremnet.bind(this);
+  }
+  incremnet() {
+    console.log(this)
+      
+    this.setState({ data: this.state.data + 1 });
+  }
+  decremnet() {
+    console.log(this)
+      
+    this.setState({ data: this.state.data - 1 });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.incremnet}>INCREMENT</button>
+        <Content myNumber={this.state.data}></Content>
+        <button onClick={this.decremnet}>DECREMENT</button>
+      </div>
+    );
+  }
+}
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>{this.props.myNumber}</h3>
+      </div>
+    );
+  }
 }
