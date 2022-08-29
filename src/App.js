0cf6e226b5bcbec3316285ab89e95fc0902e10c8
duongdo2410 <duongdo2k1@gmail.com>
2022-08-29 03:31:40 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import React, { Component } from 'react';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false
+    };
+    this.handelDisplay = this.handelDisplay.bind(this);
+  }
+  handelDisplay(){
+    let btn = !this.state.display
+    this.setState({display: btn})
+  }
+  render() {
+    if(!this.state.display) { return(
+      <div style={{ textAlign: 'center' }}>
+          <button onClick={this.handelDisplay}>hiện </button>
+      </div>
+    )}
+    return (
+      <div style={{ textAlign: 'center' }}>
+          <button btn='false'onClick={this.handelDisplay}>ẩn </button>
+          <p>content</p>
+      </div>
+    );
+  }
 }
+ 
+export default App;
