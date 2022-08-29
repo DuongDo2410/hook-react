dfa9769212706f892ce57a20e049e0f0f9520983
duongdo2410 <duongdo2k1@gmail.com>
2022-08-29 01:47:50 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./styles.css";
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import React, { Component } from 'react';
+class Hello extends Component {
+ 
+  componentWillMount() {
+    console.log("Component WILL MOUNT!");
+  }
+  componentDidMount() {
+    console.log("Component DID MOUNT!");
+  }
+  render() {
+    return <h1>Hello Word!!!</h1>;
+  }
 }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    };
+  }
+ 
+  delete = () => {
+    this.setState({ display: false });
+  };
+ 
+  render() {
+    let comp;
+    if (this.state.display) {
+      comp = <Hello />;
+    }
+    return (
+      <div style={{ textAlign: 'center' }}>
+        {comp}
+        <button onClick={this.delete}>
+          Delete the component
+        </button>
+      </div>
+    );
+  }
+}
+ 
+export default App;
